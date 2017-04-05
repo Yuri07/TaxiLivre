c5efc015066c27c07f7dc7908362936dda1cdf5f
Yuri07 <yuri_resende@hotmail.com>
2017-04-05 16:50:13 -0300

    Primeiro commit TaxiLivre.

--- app/src/main/java/com/rseng/yuri/taxilivre/fragment/DrawerFragment.java
+++ app/src/main/java/com/rseng/yuri/taxilivre/fragment/DrawerFragment.java
@@ -3,7 +3,7 @@ package com.rseng.yuri.taxilivre.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support  .annotation.Nullable;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
